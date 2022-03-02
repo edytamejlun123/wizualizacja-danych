1799f6495702f225b7736fb496b9a180cacee50a
edytamejlun123 <edytamejlun@zhr.pl>
2022-03-02 14:35:08 +0100

    XYZ

--- ćw2.py
+++ ćw2.py
@@ -1,54 +1,66 @@
-# #a = 4
-# #b = 4
-# #if a<b:
-# #    print("b jest wieksze")
-# #elif a>b:
-# #    print("a jest wieksze")
-# #else:
-# #    print("sa równe")
-#
-# # a = input('wpisz liczbę: ')
-# # print(a)
-# # print(type(a))
+# # #a = 4
+# # #b = 4
+# # #if a<b:
+# # #    print("b jest wieksze")
+# # #elif a>b:
+# # #    print("a jest wieksze")
+# # #else:
+# # #    print("sa równe")
+# #
+# # # a = input('wpisz liczbę: ')
+# # # print(a)
+# # # print(type(a))
+# # # a = int(a)
+# # # print(type(a))
+# #
+# #
+# # a = input('wpisz pierwszą liczbę: ')
+# # b = input('wpisz drugą liczbę: ')
+# # c = input('wpisz trzecią liczbę: ')
+# # d = input('wpisz czwartą liczbę: ')
 # # a = int(a)
-# # print(type(a))
+# # b=int(b)
+# # c=int(c)
+# # d=int(d)
+# # if(a>b)&(c>d):
+# #     print('liczba a jest wieksza od b i c jest wieksze od d')
+# # else:
+# #     print('a jest mniejsze od b lub c jest mniejsze od d')
+# #
 #
+# #for licznik in sekwencja:
+#     #instrukcja lub blok instrukcji
+# #else :
+#     #instrukcja po wykonaniu pętli
 #
-# a = input('wpisz pierwszą liczbę: ')
-# b = input('wpisz drugą liczbę: ')
-# c = input('wpisz trzecią liczbę: ')
-# d = input('wpisz czwartą liczbę: ')
-# a = int(a)
-# b=int(b)
-# c=int(c)
-# d=int(d)
-# if(a>b)&(c>d):
-#     print('liczba a jest wieksza od b i c jest wieksze od d')
-# else:
-#     print('a jest mniejsze od b lub c jest mniejsze od d')
+# # for i in range(1,7,1):
+# #     print(i)
 #
-
-#for licznik in sekwencja:
-    #instrukcja lub blok instrukcji
-#else :
-    #instrukcja po wykonaniu pętli
-
-# for i in range(1,7,1):
-#     print(i)
-
-# lista = ['a', 5,6,5.6]
-# for a in lista:
+# # lista = ['a', 5,6,5.6]
+# # for a in lista:
+# #     print(a)
+# # else:
+# #     print('wyświetlone zostały wszystkie elementy z listy')
+# #
+# # while warunek:
+# #     instrukcja lub blok instrukcji, gdy warunek spełniony
+# # else:
+# #     instrukcje po pętli
+# a=0
+# while a<10:
+#     a+=1
 #     print(a)
 # else:
-#     print('wyświetlone zostały wszystkie elementy z listy')
-#
-# while warunek:
-#     instrukcja lub blok instrukcji, gdy warunek spełniony
-# else:
-#     instrukcje po pętli
-a=0
-while a<10:
-    a+=1
-    print(a)
+#     print('liczba wieksza od 10')
+
+lista = [4, 6, 9, 5, 7, 2, 3]
+liczba = input('podaj liczbę: ')
+licznik=0
+while licznik<len(lista):
+    if int(liczba) - lista[licznik] == 0:
+        print('liczba ' + str(liczba) + ' - ' + str(lista[licznik]) + ' = 0' )
+        break
+    else:
+        licznik+=1
 else:
-    print('liczba wieksza od 10')
+    print('żadna z wartości nie dała odpowiedniego wyniku')
