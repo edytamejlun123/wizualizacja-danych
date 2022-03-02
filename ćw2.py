cd1b185bd20d5f078e1d4538cd7bf8f737787f9e
edytamejlun123 <edytamejlun@zhr.pl>
2022-03-02 14:28:22 +0100

    XYZ

---
+++ ćw2.py
@@ -0,0 +1,54 @@
+# #a = 4
+# #b = 4
+# #if a<b:
+# #    print("b jest wieksze")
+# #elif a>b:
+# #    print("a jest wieksze")
+# #else:
+# #    print("sa równe")
+#
+# # a = input('wpisz liczbę: ')
+# # print(a)
+# # print(type(a))
+# # a = int(a)
+# # print(type(a))
+#
+#
+# a = input('wpisz pierwszą liczbę: ')
+# b = input('wpisz drugą liczbę: ')
+# c = input('wpisz trzecią liczbę: ')
+# d = input('wpisz czwartą liczbę: ')
+# a = int(a)
+# b=int(b)
+# c=int(c)
+# d=int(d)
+# if(a>b)&(c>d):
+#     print('liczba a jest wieksza od b i c jest wieksze od d')
+# else:
+#     print('a jest mniejsze od b lub c jest mniejsze od d')
+#
+
+#for licznik in sekwencja:
+    #instrukcja lub blok instrukcji
+#else :
+    #instrukcja po wykonaniu pętli
+
+# for i in range(1,7,1):
+#     print(i)
+
+# lista = ['a', 5,6,5.6]
+# for a in lista:
+#     print(a)
+# else:
+#     print('wyświetlone zostały wszystkie elementy z listy')
+#
+# while warunek:
+#     instrukcja lub blok instrukcji, gdy warunek spełniony
+# else:
+#     instrukcje po pętli
+a=0
+while a<10:
+    a+=1
+    print(a)
+else:
+    print('liczba wieksza od 10')
