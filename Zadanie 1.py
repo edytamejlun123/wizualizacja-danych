2c441dfd07e32a6035a37d19fd9fe13898e888c1
edytamejlun123 <edytamejlun@zhr.pl>
2022-03-02 13:33:21 +0100

    XYZ

--- Zadanie 1.py
+++ Zadanie 1.py
@@ -1,3 +1,4 @@
+#Zad 1
 a = 6
 b = 8
 print(a+b)
@@ -9,3 +10,48 @@ e = 3+5j
 print(e)
 f = 2+2j
 print(f+e+1)
+
+#Zad2
+a=8
+b=4
+c=3
+print(a/b)
+print(a/c)
+print(a+b)
+print(a//c)
+print(a%b)
+print(a%c)
+print(b/a**c)
+d=pow(2,3)
+e=a/b
+f=pow(e,c)
+print(f)
+
+#Zad3
+#zad3
+a=2
+b=3
+a+=b
+print(a)
+c=2
+d=5
+d-=c
+print(d)
+
+e = 4
+f = 2
+e*=f
+print(e)
+
+h=4
+h/=f
+print(h)
+
+g = 4
+g**=f
+print(g)
+
+i =7
+t=3
+i%=t
+print(i)
