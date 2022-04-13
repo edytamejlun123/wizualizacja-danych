438fb713d223daf06b7fda8341a87c22ad868024
edytamejlun123 <92027759+edytamejlun123@users.noreply.github.com>
2022-04-13 14:14:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import numpy as np
+# a = np.arange(4)
+# print(a)
+# b = np.array([2,3,4,5])
+# print(b)
+# print(b**2)
+# print(np.sqrt(b))
+# print(np.exp(b))
+#
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# print(a.sum())
+# print(a.sum(axis=0))
+# print(a.sum(axis=1))
+#
+# print(a.cumsum(axis=1))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# a = np.arange(12).reshape((3,4))
+# print(a)
+# print(a.sum())
 
+# a = np.arange(3)
+# b = np.arange(3)
+#
+# c = a.dot(b)
+# print(c)
+#
+# d = np.array([[3, 4, 1], [4, 6, 2]])
+# e = np.array([[2, 4], [1, 3], [2, 5]])
+# f = d.dot(e)
+# print(f)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# for b in a:
+#     print(b)
+#
+# for c in a.flat:
+#     print(c)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# a = np.arange(12)
+# print(a)
+# b = a.reshape((3,4))
+# c = a.reshape((4,3))
+# print(b)
+# print(c)
+# d = c.ravel()
+# print(d)
+# e = c.T
+# print(e)
